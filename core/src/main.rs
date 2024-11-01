c0237afb98bc269753ae4db96bed76234ff0a28b
Ekrem BAL <mail.ekrembal@gmail.com>
2024-11-01 20:43:19 +0700

    Fix risc0_zkvm import

--- core/src/main.rs
+++ core/src/main.rs
@@ -5,36 +5,6 @@ use circuits::{
 };
 use header_chain_circuit::{HEADER_CHAIN_GUEST_ELF, HEADER_CHAIN_GUEST_ID};
 
-
-pub struct Risc0Guest;
-
-impl Risc0Guest {
-    pub fn new() -> Self {
-        Self {}
-    }
-}
-
-impl ZkvmGuest for Risc0Guest {
-    fn read_from_host<T: borsh::BorshDeserialize>(&self) -> T {
-        let mut reader = env::stdin();
-        // deserialize
-        BorshDeserialize::deserialize_reader(&mut reader)
-            .expect("Failed to deserialize input from host")
-    }
-
-    fn commit<T: borsh::BorshSerialize>(&self, item: &T) {
-        // use risc0_zkvm::guest::env::Write as _;
-        let buf = borsh::to_vec(item).expect("Serialization to vec is infallible");
-        let mut journal = env::journal();
-        journal.write_slice(&buf);
-    }
-
-    fn verify(&self, method_id: [u32; 8], journal: &[u32]) {
-        env::verify(method_id, journal).unwrap();
-    }
-}
-
-
 fn main() {
     // Download the headers.bin file from https://zerosync.org/chaindata/headers.bin
     let headers = include_bytes!("../../headers.bin");
--- guests/header-chain/guest/src/main.rs
+++ guests/header-chain/guest/src/main.rs
@@ -1,7 +1,41 @@
 #![no_main]
-circuits::risc0_zkvm::guest::entry!(main);
+
+use borsh::BorshDeserialize;
+use circuits::ZkvmGuest;
+use risc0_zkvm::guest::env::{self, Write};
+risc0_zkvm::guest::entry!(main);
+
+
+pub struct Risc0Guest;
+
+impl Risc0Guest {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl ZkvmGuest for Risc0Guest {
+    fn read_from_host<T: borsh::BorshDeserialize>(&self) -> T {
+        let mut reader = env::stdin();
+        // deserialize
+        BorshDeserialize::deserialize_reader(&mut reader)
+            .expect("Failed to deserialize input from host")
+    }
+
+    fn commit<T: borsh::BorshSerialize>(&self, item: &T) {
+        // use risc0_zkvm::guest::env::Write as _;
+        let buf = borsh::to_vec(item).expect("Serialization to vec is infallible");
+        let mut journal = env::journal();
+        journal.write_slice(&buf);
+    }
+
+    fn verify(&self, method_id: [u32; 8], journal: &[u32]) {
+        env::verify(method_id, journal).unwrap();
+    }
+}
+
 
 fn main() {
-    let zkvm_guest = circuits::Risc0Guest::new();
+    let zkvm_guest = Risc0Guest::new();
     circuits::header_chain::header_chain_circuit(&zkvm_guest);
 }
