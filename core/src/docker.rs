1910800bdae4dd50d165424673f57584ec8acbb5
Ekrem BAL <mail.ekrembal@gmail.com>
2024-11-04 14:37:03 +0700

    Fix seal

--- core/src/docker.rs
+++ core/src/docker.rs
@@ -1,21 +1,43 @@
 use crypto_bigint::U256;
 use hex::ToHex;
 use num_bigint::BigUint;
-use risc0_groth16::to_json;
-use risc0_zkvm::{ReceiptClaim, SuccinctReceipt, SuccinctReceiptVerifierParameters};
-use serde_json::Value;
 use num_traits::Num;
+use risc0_groth16::{to_json, ProofJson, Seal};
+use risc0_zkvm::{Receipt, ReceiptClaim, SuccinctReceipt, SuccinctReceiptVerifierParameters};
+use serde_json::Value;
 
-use std::{fs, path::Path, process::{Command, Stdio}};
+use std::{
+    env::consts::ARCH, fs, path::Path, process::{Command, Stdio}
+};
 
 use tempfile::tempdir;
 
-
 pub fn stark_to_succinct(
-    ident_receipt: &SuccinctReceipt<ReceiptClaim>,
+    succinct_receipt: &SuccinctReceipt<ReceiptClaim>,
     journal: &[u8],
     verify_stark_method_id: &[u32],
-) {
+) -> Seal {
+    // let succinct_receipt: &SuccinctReceipt<ReceiptClaim> = receipt.inner.succinct().unwrap();
+    let ident_receipt = risc0_zkvm::recursion::identity_p254(succinct_receipt).unwrap();
+    let identity_p254_seal_bytes = ident_receipt.get_seal_bytes();
+    // if !is_x86_architecture() {
+    //     panic!("stark_to_snark is only supported on x86 architecture.")
+    // }
+    if !is_docker_installed() {
+        panic!("Please install docker first.")
+    }
+
+    let tmp_dir = tempdir().unwrap();
+    let work_dir = std::env::var("RISC0_WORK_DIR");
+    let work_dir = work_dir.as_ref().map(Path::new).unwrap_or(tmp_dir.path());
+
+    std::fs::write(work_dir.join("seal.r0"), identity_p254_seal_bytes.clone()).unwrap();
+    let seal_path = work_dir.join("input.json");
+    let proof_path = work_dir.join("proof.json");
+    let mut seal_json = Vec::new();
+    to_json(&*identity_p254_seal_bytes, &mut seal_json).unwrap();
+    std::fs::write(seal_path.clone(), seal_json).unwrap();
+
     let mut pre_state_bits: Vec<u8> = Vec::new();
     for item in verify_stark_method_id.iter().take(8) {
         for j in 0..4 {
@@ -24,20 +46,7 @@ pub fn stark_to_succinct(
             }
         }
     }
-    let tmp_dir = tempdir().unwrap();
-    let work_dir = std::env::var("RISC0_WORK_DIR");
-    let work_dir = work_dir.as_ref().map(Path::new).unwrap_or(tmp_dir.path());
-    let identity_p254_seal_bytes = ident_receipt.get_seal_bytes();
-    std::fs::write(
-        work_dir.join("seal.r0"),
-        identity_p254_seal_bytes.as_slice(),
-    )
-    .unwrap();
-    let seal_path = work_dir.join("input.json");
-    let _proof_path = work_dir.join("proof.json");
-    let mut seal_json = Vec::new();
-    to_json(identity_p254_seal_bytes.as_slice(), &mut seal_json).unwrap();
-    std::fs::write(seal_path.clone(), seal_json).unwrap();
+
     let journal_hex = hex::encode(journal);
     println!("Journal hex: {:?}", journal_hex);
 
@@ -115,6 +124,11 @@ pub fn stark_to_succinct(
             output.status.code()
         );
     }
+
+    let contents = std::fs::read_to_string(proof_path).unwrap();
+    let proof_json: ProofJson = serde_json::from_str(&contents).unwrap();
+    proof_json.try_into().unwrap()
+
 }
 
 pub fn bits_to_num(len: usize, bits: &[u8]) -> U256 {
@@ -144,4 +158,16 @@ pub fn bits_to_num(len: usize, bits: &[u8]) -> U256 {
 pub fn to_decimal(s: &str) -> Option<String> {
     let int = BigUint::from_str_radix(s, 16).ok();
     int.map(|n| n.to_str_radix(10))
+}
+
+fn is_docker_installed() -> bool {
+    Command::new("docker")
+        .arg("--version")
+        .output()
+        .map(|output| output.status.success())
+        .unwrap_or(false)
+}
+
+fn is_x86_architecture() -> bool {
+    ARCH == "x86_64" || ARCH == "x86"
 }
