8773a58339fc6bc661e646f81a53d840c05860e8
ozankaymak <ozan.kaymak@boun.edu.tr>
2024-11-22 17:18:40 +0300

    Implement mmr for both guest and native with tests

--- circuits/src/utils.rs
+++ circuits/src/utils.rs
@@ -7,3 +7,17 @@ pub fn calculate_double_sha256(input: &[u8]) -> [u8; 32] {
     hasher.update(result);
     hasher.finalize().into()
 }
+
+pub fn calculate_sha256(input: &[u8]) -> [u8; 32] {
+    let mut hasher = Sha256::default();
+    hasher.update(input);
+    hasher.finalize().into()
+}
+
+/// Utility function to hash two nodes together
+pub fn hash_pair(left: [u8; 32], right: [u8; 32]) -> [u8; 32] {
+    let mut data = [0u8; 64];
+    data[..32].copy_from_slice(&left);
+    data[32..].copy_from_slice(&right);
+    calculate_sha256(&data)
+}
