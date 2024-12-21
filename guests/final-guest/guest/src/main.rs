03c8e862b41bbaf9daed459e263a54936a194f98
ozankaymak <ozan.kaymak@boun.edu.tr>
2024-12-21 15:00:17 +0300

    Remove unused guests

--- guests/final-guest/guest/src/main.rs
+++
@@ -1,6 +0,0 @@
-#![no_main]
-circuits::risc0_zkvm::guest::entry!(main);
-fn main() {
-    let zkvm_guest = circuits::zkvm::Risc0Guest::new();
-    circuits::final_circuit(&zkvm_guest);
-}
--- guests/header-chain-guest/guest/src/main.rs
+++
@@ -1,6 +0,0 @@
-#![no_main]
-circuits::risc0_zkvm::guest::entry!(main);
-fn main() {
-    let zkvm_guest = circuits::zkvm::Risc0Guest::new();
-    circuits::header_chain_circuit(&zkvm_guest);
-}
